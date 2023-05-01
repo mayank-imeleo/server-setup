5cc6a9a5775c1a499abaf5aad6d460343c1da502
Mayank Mahajan <mayankm2089@gmail.com>
2023-05-01 17:55:35 +0530

    Added script for python3.9

---
+++ scripts/install_python39.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install software-properties-common
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9
+
+sudo pip install virtualenv
